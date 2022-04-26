000225d1784133e1f2d84dc1b1844250bfa5838a
Pthumeriano <helderandrade377@gmail.com>
2022-04-26 11:37:30 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -73,6 +73,21 @@ class _HomeState extends State<Home> {
     }
   }
 
+  void sortear(){
+    List numeros = [1,2,3,4,5,6,7,8,9,10,
+                 11,12,13,14,15,16,18,18,19,20,
+                 21,22,23,24,25,26,27,28,29,30,
+                 31,32,33,34,35,36,37,38,39,40,
+                 41,42,43,44,45,46,47,48,49,50,
+                 51,52,53,54,55,56,57,58,59,60];
+    int indice;
+    for(var i=0; i<6; i++){
+      indice = Random().nextInt(numeros.length);
+      numerosSorteados.add(numeros[indice]);
+      numeros.removeAt(indice);
+    }
+  }
+
   void ordenar(x){
     int aux;
     for(int i = 0; i < x.length - 1; i++){
@@ -128,7 +143,8 @@ class _HomeState extends State<Home> {
       b41a = false; b42a = false; b43a = false; b44a = false; b45a = false; b46a = false; b47a = false; b48a = false; b49a = false; b50a = false;
       b51a = false; b52a = false; b53a = false; b54a = false; b55a = false; b56a = false; b57a = false; b58a = false; b59a = false; b60a = false;
 
-      gerarAleatorios();
+      //gerarAleatorios();
+      sortear();
 
       if(contadorEscolhidos != 6){
         mensagem = "Quantidade de números inválida";
